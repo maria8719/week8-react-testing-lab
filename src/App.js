65d6e2c5f5cd68511553e51d3eac2eb6464ffa29
Frank Stepanski <fsjay@hotmail.com>
2022-04-02 00:35:49 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       Hello World
+      <button>this is a button</button>
+      <div className="blue-div" style={{color: 'red'}}>this div is blue</div>
     </div>
   );
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,23 +6,43 @@ test('renders Hello World', () => { // test block and description
   // Rendering the App component (need to render before we can test):
   render(<App />);
   // Finding the element "Hello World" in the DOM
-  // note: ($ means end of the string; i means case insensitive):
-  const elemText = screen.getByText(/Hello World$/i); 
+  // https://testing-library.com/docs/queries/bytext/
+  // note: you can pass in regular expressions if you want
+  // $ means end of the string; i means case insensitive:
+  const elemText = screen.getByText(/hello world$/i); 
   // Assertion - expect the element to be in the document:
   // note: assertions always pass unless an error is thrown
   expect(elemText).toBeInTheDocument();
 });
 
 // Test #2
-test('button has correct color', () => {
-  // rendering the App component:
+test('button has text of this is a button', () => {
+  render(<App />); 
+  // most elements have a built in aria role
+  // https://www.w3.org/TR/html-aria/#docconformance
+  const button = screen.getByRole('button');
+  expect(button).toBeInTheDocument();
+});
+
+// Test #3
+test('div has className of blue-div', () => {
   render(<App />); 
   // finding the button element:
   // https://testing-library.com/docs/queries/byrole/
-  const button = screen.getByRole('button');
-  // https://testing-library.com/docs/ecosystem-jest-dom/
-  // https://github.com/testing-library/jest-dom#tohavestyle
-  // asserting the button color:
-  expect(button).toHaveStyle('color: blue');
+  const div = screen.getByText(/this div is blue$/i);
+  // asserting the div has className of blue-div':
+  // https://github.com/testing-library/jest-dom#tohaveclass
+  expect(div).toHaveClass('blue-div');
 });
 
+// Test #4
+test('div has className of blue-div', () => {
+  render(<App />); 
+  const div = screen.getByText(/this div is blue$/i);
+  // asserting the div has color style of red
+  // note: You cannot assert external styles, only internal styles
+  // https://github.com/testing-library/jest-dom#tohavestyle
+  expect(div).toHaveStyle({
+    color: 'red'
+  })    
+});
